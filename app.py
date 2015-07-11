63c722ad4ab4cf819e2f9123444d738b96516521
revlino <mark@revly.net>
2015-07-11 18:51:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import sys
 import web
 import modules.gpio
@@ -8,10 +9,38 @@ import modules.adc
 urls = (
 	'/gpio(.*)', 'GPIO',
 	'/adc(.*)', 'ADC',
+	'/(.*)', 'Docs',
 	
 )
 app = web.application(urls, globals())
 
+class Docs:
+	
+	def __init__(self,directory='/opt/lininoIO-REST/manual'):
+		
+		self.manual = ''
+		
+		items = os.listdir(directory)
+
+		for item in items:
+			
+			if os.path.isdir("%s/%s" % (directory,item)):
+				self.__init__(directory+"/"+item)
+			
+			if item.endswith(".txt"):
+				
+				f = open(directory+"/"+item,'r')
+				self.manual += f.read()
+				f.close()
+		
+		return 
+	
+	
+	def GET(self, args):
+		
+		return self.manual
+		
+		
 class GPIO:
 
 	def __init__(self):
@@ -158,15 +187,39 @@ class ADC:
 					
 				except:
 					return sys.exc_info()[1]
-
+			
 			return self.manual		
 			
-		else:
-				
+		elif args[0].lower() == 'scale':
+			
 			if len(args) == 2:
-
+				
 				try:
-					return modules.adc.read_value(*args[1:])
+					return modules.adc.read_scale(*args[1:])
+					
+				except:
+					return sys.exc_info()[1]
+			
+			return self.manual	
+			
+		elif args[0].lower() == 'voltage':
+			
+			if len(args) == 2:
+				
+				try:
+					return modules.adc.read_voltage(*args[1:])
+					
+				except:
+					return sys.exc_info()[1]
+			
+			return self.manual	
+			
+		else:
+			
+			if len(args) == 1:
+				
+				try:
+					return modules.adc.read_value(*args)
 					
 				except:
 					return sys.exc_info()[1]
