dd5e4383b927e94435469f4f85bca1e456625430
revlino <mark@revly.net>
2015-07-11 17:03:12 -0500

    Create adc.py

---
+++ modules/adc.py
@@ -0,0 +1,88 @@
+# Python module for controlling Arduino Yun Analog pins
+
+class LininoIOError(Exception):
+	pass
+
+class DeviceError(LininoIOError):
+	pass
+
+class PinError(LininoIOError):
+	pass
+
+
+def pin_map():
+
+	map = {
+		0: {'id': 0, 'name': 'A0'},
+		1: {'id': 1, 'name': 'A1'},
+		2: {'id': 2, 'name': 'A2'},
+		3: {'id': 3, 'name': 'A3'},
+		4: {'id': 4, 'name': 'A4'},
+		5: {'id': 5, 'name': 'A5'},
+	}
+	
+	return map
+
+
+def get_pin(pin):
+	
+	# vars
+	pins = pin_map()
+	
+	if isinstance(pin,basestring):
+		
+		pin = pin.lower().replace('a','')
+		
+		if not pin.isdigit():
+			
+			raise PinError("Pin '%s' is not an analog pin." % pin)
+			
+		pin = int(pin)
+	
+	if isinstance(pin,int):
+		
+		if not pins[pin]['id']:
+	
+			raise PinError("Pin '%d' is not an analog pin." % pin)
+			
+		return pin
+			
+	else:
+		
+		print(type(pin))
+		
+		raise PinError("'%s' is not an analog pin." % str(pin))
+
+
+def export():
+	
+	try:
+		f.open('/sys/bus/iio/devices/iio:device0/enable', 'w')
+		f.write('1')
+		f.close()
+	
+	except IOError:
+		
+		raise DeviceError("Device not found.")	
+	
+	return None
+
+	
+def read_value(pin):
+
+	# vars
+	pins = pin_map()
+	pin = get_pin(pin)
+	
+	try:
+	
+		# read value
+		f = open('/sys/bus/iio/devices/iio:device0/in_voltage_%s_raw' % pins[pin]['name'],'r')
+		value = f.read().strip()
+		f.close()
+	
+	except IOError:
+		
+		raise PinError("Analog pins are not enabled.")
+
+	return value	
